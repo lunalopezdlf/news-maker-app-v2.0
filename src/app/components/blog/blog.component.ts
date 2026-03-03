e531baece7a55fc9abf6940ec2be0ba3a4835108
LunaLopez <luna.lopezdlf@gmail.com>
2026-03-03 18:27:38 +0100

    feat: implementar el formulario
    con validación e inserción de noticias dinámicas

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -2,13 +2,28 @@ import { Component, signal } from '@angular/core';
 import { INew } from '../../interfaces/inew.interface';
 import { NEWS } from '../../db/news.db';
 import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-blog',
-  imports: [DatePipe],
+  imports: [DatePipe, FormsModule],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css',
 })
 export class BlogComponent {
-    newsList = signal<INew[]>(NEWS)
+    myNews = signal<INew[]>(NEWS)
+
+    newItem: INew = { title: "", image: "", date: "", text: "" };
+
+    addNews():void{
+        console.log(this.addNews); //comprobación
+
+        if ( this.newItem.title != "" && this.newItem.image != "" && this.newItem.date != "" &&  this.newItem.text != ""){
+            this.myNews.update( (news) => [...news, this.newItem])
+            this.newItem = { title: "", image: "", date: "", text: "" };
+        }else{
+            alert("Todos los campos son obligatorios.");
+            return;
+        }
+    }
 }
